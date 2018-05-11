cf5d46b8c16e0cb44e6c9e00812c545fb7627884
Sylar <yuliang0225@sina.com>
2018-05-11 14:56:51 +0900

    Add --exc 1.1.9

--- Chapter01/src/Exc_1_1.java
+++ Chapter01/src/Exc_1_1.java
@@ -8,6 +8,9 @@ public class Exc_1_1 {
         exc_1_1_3(2.0,2,2);
         exc_1_1_5(1,0.5);
         exc_1_1_6();
+        exc_1_1_7();
+        exc_1_1_8();
+        exc_1_1_9(18);
 
     }
 
@@ -57,6 +60,67 @@ public class Exc_1_1 {
             g=f-g;
         }
     }
+
+    public static void exc_1_1_7(){
+        System.out.println("1.1.7 a:");
+        double t = 9.0;
+        while (Math.abs(t-9.0/t)>.001)
+            t = (9.0/t + t) / 2.0;
+        StdOut.printf("%.5f\n",t);
+
+        System.out.println("1.1.7 b:");
+        int sum = 0;
+        for (int i = 1; i < 1000; i++)
+            for (int j = 1; j < 1000; j++)
+                sum++;
+        StdOut.println(sum);
+
+        System.out.println("1.1.7 c:");
+        sum = 0;
+        for (int i = 1; i < 1000; i *= 2)
+            for (int j = 1; j < 1000; j++)
+                sum++;
+        StdOut.println(sum);
+    }
+
+    public static void exc_1_1_8(){
+        System.out.println("1.1.8 a:");
+        System.out.println('b');
+
+        System.out.println("1.1.8 b:");
+        System.out.println('b'+'c');
+
+        System.out.println("1.1.8 c:");
+        System.out.println((char)('a'+4));
+
+    }
+
+    public static void exc_1_1_9(int N){
+        System.out.println("1.1.9:");
+
+        String s = "";
+        for (int n = N; n > 0; n /= 2)
+            s = (n % 2)+s;
+
+        System.out.println(s);
+    }
+
+    public static void exc_1_1_10(){
+        System.out.println("1.1.10:");
+
+        String s = "";
+        for (int n = N; n > 0; n /= 2)
+            s = (n % 2)+s;
+
+        System.out.println(s);
+    }
+
+    
+
+
+
+
+
 
 
 
